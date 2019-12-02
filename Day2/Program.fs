8e2dac8de277cd701907df60d6f7b47836405949
Tiago Abreu <tiagoalvesabreu@gmail.com>
2019-12-02 16:00:04 +0100

    Day 2 - Challenges 1 and 2

---
+++ Day2/Program.fs
@@ -0,0 +1,42 @@
+﻿module Program
+
+open Common.Files
+
+let resolveValue (operations:int[]) i =
+    operations.[operations.[i]]
+
+let setValue (operations:int[]) value i =
+    operations.[operations.[i]] <- value
+
+let ProcessOperation operation operations i =
+    let operand1 = resolveValue operations <| i + 1
+    let operand2 = resolveValue operations <| i + 2
+    setValue operations (operation operand1 operand2) (i + 3)
+    operations
+
+let rec ProcessIntCode i (operations:int[]) =
+    let operation = operations.[i]
+    match operation with
+        | 99 -> operations
+        | 1 -> ProcessOperation (fun x y -> x + y) operations i
+               |> ProcessIntCode (i + 4)
+        | 2 -> ProcessOperation (fun x y -> x * y) operations i
+               |> ProcessIntCode (i + 4)
+        | _ -> operations
+        
+
+let CalculateIntCode operations =
+    operations
+    |> ProcessIntCode 0
+
+let ReplaceValues replacements (operations:int[]) =
+    for replacement in replacements do
+        operations.[fst replacement] <- snd replacement
+    operations
+
+let CalculateIntCodeFromFile filePath replacements =
+    (getLineValuesFromFilePath filePath).[0].Split ','
+    |> Array.map int
+    |> ReplaceValues replacements
+    |> CalculateIntCode
+
---
+++ Day2/Tests.fs
@@ -0,0 +1,34 @@
+﻿module Tests
+
+open Xunit
+open Program
+open System
+
+[<Theory>]
+[<InlineData("1,0,0,0,99", "2,0,0,0,99")>]
+[<InlineData("2,3,0,3,99", "2,3,0,6,99")>]
+[<InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")>]
+[<InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")>]
+let ``Calculate IntCode Test Cases`` ((input:string), expected) =
+
+    let result = CalculateIntCode (Array.map int
+                 <| input.Split ',')
+
+    //let result = CalculateIntCode input
+    Assert.Equal (expected, String.Join (',', result))
+
+[<Fact>]
+let ``Real Case Calculate IntCode`` () =
+    let replacements = seq { (1, 12); (2, 2) }
+    let result = CalculateIntCodeFromFile "input.txt" replacements
+    Console.WriteLine result.[0]
+
+[<Fact>]
+let ``Real Case Calculate IntCode and Find Result`` () =
+
+    for i in 0 .. 99 do
+        for j in 0 .. 99 do
+            let replacements = seq { (1, i); (2, j) }
+            let result = CalculateIntCodeFromFile "input.txt" replacements
+            if (result.[0] = 19690720) then
+                printfn "%i" (100 * i + j)
